148d0b489637f6b4f2776f5e5a0d24247c49a86c
Downvote Hunter <reschsaid@gmail.com>
2022-12-12 21:12:34 +0100

    remove serialVersionUID

--- src/praktikum3/ColumnTypeNotFoundException.java
+++ src/praktikum3/ColumnTypeNotFoundException.java
@@ -2,7 +2,6 @@ package praktikum3;
 
 public class ColumnTypeNotFoundException extends Exception {
 
-    private static final long serialVersionUID = 1L;
 
     public ColumnTypeNotFoundException(String errorMessage) {
         super(errorMessage);
